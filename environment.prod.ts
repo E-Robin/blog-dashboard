de76d6609e33deee9caa3e0bc1b172831a4e483d
E-Robin <grobin578@gmail.com>
2023-08-20 16:19:38 +0530

    firebase intitialization

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,13 +1,25 @@
+// export const env = {
+//     firebaseConfig : 
+//         {
+//             apiKey: "AIzaSyAEppmskLJ9nr9lYXRe5YAkhevEuFouk2U",
+//             authDomain: "app-new-e8d41.firebaseapp.com",
+//             projectId: "app-new-e8d41",
+//             storageBucket: "app-new-e8d41.appspot.com",
+//             messagingSenderId: "543182387334",
+//             appId: "1:543182387334:web:bedca507c6cbc373a27907"
+//           }
+    
+    
+// }
+
 export const env = {
-    firebaseConfig : 
-        {
-            apiKey: "AIzaSyAEppmskLJ9nr9lYXRe5YAkhevEuFouk2U",
-            authDomain: "app-new-e8d41.firebaseapp.com",
-            projectId: "app-new-e8d41",
-            storageBucket: "app-new-e8d41.appspot.com",
-            messagingSenderId: "543182387334",
-            appId: "1:543182387334:web:bedca507c6cbc373a27907"
-          }
-    
-    
+     firebaseConfig : {
+        apiKey: "AIzaSyA1hMRfO8TszWlnnf6V4o7YbBlR5RmRQSQ",
+        authDomain: "blog-app-639b2.firebaseapp.com",
+        projectId: "blog-app-639b2",
+        storageBucket: "blog-app-639b2.appspot.com",
+        messagingSenderId: "515846174319",
+        appId: "1:515846174319:web:eea55b5ca30c08dd24bf9a"
+      }
+
 }
